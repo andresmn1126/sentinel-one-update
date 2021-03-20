80083450fd04656a7d844b30aba6eb803891b395
Andres Martinez <amartinez@andymartinez.tech>
2021-03-20 11:20:02 -0400

    Modified main() to use map instead of repeating the function calls twice for each os type

--- update_agents.py
+++ update_agents.py
@@ -7,10 +7,10 @@ api_token = "ApiToken " + getenv('SENTINEL_ONE_API')
 headers = {'Authorization': api_token}
 
 
-def get_os_package(os_type, pkg_type):
+def get_os_package(os_type):
     url = 'https://usea1-007.sentinelone.net/web/api/v2.1/update/agent/packages'
     res = requests.get(url, headers=headers, params={'limit': 1000}).json()['data']
-    packages = [i for i in res if i['osType'] == os_type and i['status'] == 'ga' and i['fileExtension'] == pkg_type]
+    packages = [i for i in res if i['osType'] == os_type and i['status'] == 'ga' and i['fileExtension'] != '.msi']
     sorted_packages = sorted(packages, key=itemgetter('version'), reverse=True)
     return sorted_packages[0]
 
@@ -35,18 +35,18 @@ def update_agents(agents, ver_id):
         try:
             json_data = {'data': {'packageId': ver_id, 'isScheduled': True}, 'filter': {'uuid': a['uuid']}}
             update = requests.post(url, headers=headers, json=json_data)
+            update.raise_for_status()
             print(f"Update on {a['computerName']} under {a['siteName']} successfully triggered")
-        except:
-            print(update.status_code)
+        except requests.exceptions.HTTPError as e:
+            print(e)
 
 
 def main():
-    win_id = get_os_package('windows', '.exe')
-    mac_id = get_os_package('macos', '.pkg')
-    win_agents = get_outdated_agents('windows', win_id['version'])
-    mac_agents = get_outdated_agents('macos', mac_id['version'])
-    update_agents(win_agents, win_id['id'])
-    update_agents(mac_agents, mac_id['id'])
+    os_types = ['windows', 'macos']
+    winid, macid = map(get_os_package, os_types)
+    winagents, macagents = map(get_outdated_agents, os_types, [winid['version'], macid['version']])
+    update_agents(winagents, winid['id'])
+    update_agents(macagents, macid['id'])
 
 
 if __name__ == '__main__':
