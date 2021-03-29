0f526af95294e22bd2e402a5567cc99786300018
Andres Martinez <amartinez@andymartinez.tech>
2021-03-29 09:47:21 -0400

    Chnaged year format on filename to include full year, added downloads path to file

--- get_csv.py
+++ get_csv.py
@@ -7,22 +7,22 @@ import csv
 api_token = "ApiToken " + getenv('SENTINEL_ONE_API')
 CMS_SITE_ID = getenv('CMS_SITE_ID')
 headers = {'Authorization': api_token}
-now = datetime.now().strftime("%m-%d-%y")
+now = datetime.now().strftime("%m-%d-%Y")
 
 
 def export_agents_to_csv():
     url = 'https://usea1-007.sentinelone.net/web/api/v2.1/export/agents'
     res = requests.get(url, headers=headers, params={'siteIds': [CMS_SITE_ID]})
-    with open(f'CMS-CO_S1_Master_{now}.csv', 'w') as org:
+    with open(f'./downloads/CMS-CO_S1_Master_{now}.csv', 'w') as org:
         org.write(res.text)
     return res.text
 
 
 def format_scan():
-    with open(f'CMS-CO_S1_Master_{now}.csv', 'r') as org:
+    with open(f'./downloads/CMS-CO_S1_Master_{now}.csv', 'r') as org:
         reader = csv.reader(org)
 
-        with open(f'CMS-CO_S1_ScanStatus_{now}.csv', 'w') as modified:
+        with open(f'./downloads/CMS-CO_S1_ScanStatus_{now}.csv', 'w') as modified:
             writer = csv.writer(modified)
             for r in reader:
                 writer.writerow((r[0], r[22]))
