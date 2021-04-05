6fcc1d9ba2ac806c37f3e2a4e9053ffc1523fbf7
Andres Martinez <amartinez@andymartinez.tech>
2021-04-05 10:40:02 -0400

    Entered step to create downloads directory if it does not exist already

--- get_csv.py
+++ get_csv.py
@@ -1,28 +1,32 @@
 import requests
-from os import getenv
+import os
 from datetime import datetime
+from pathlib import Path
 import csv
 
 
-api_token = "ApiToken " + getenv('SENTINEL_ONE_API')
-CMS_SITE_ID = getenv('CMS_SITE_ID')
+api_token = "ApiToken " + os.getenv('SENTINEL_ONE_API')
+CMS_SITE_ID = os.getenv('CMS_SITE_ID')
 headers = {'Authorization': api_token}
+download_dir = f'{Path.home()}/sentinel-one/downloads/'
 now = datetime.now().strftime("%m-%d-%Y")
 
 
 def export_agents_to_csv():
     url = 'https://usea1-007.sentinelone.net/web/api/v2.1/export/agents'
     res = requests.get(url, headers=headers, params={'siteIds': [CMS_SITE_ID]})
-    with open(f'./downloads/CMS-CO_S1_Master_{now}.csv', 'w') as org:
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+    with open(f'{download_dir}CMS-CO_S1_Master_{now}.csv', 'w') as org:
         org.write(res.text)
     return res.text
 
 
-def format_scan():
-    with open(f'./downloads/CMS-CO_S1_Master_{now}.csv', 'r') as org:
+def format_csv():
+    with open(f'{download_dir}CMS-CO_S1_Master_{now}.csv', 'r') as org:
         reader = csv.reader(org)
 
-        with open(f'./downloads/CMS-CO_S1_ScanStatus_{now}.csv', 'w') as modified:
+        with open(f'{download_dir}CMS-CO_S1_ScanStatus_{now}.csv', 'w') as modified:
             writer = csv.writer(modified)
             for r in reader:
                 writer.writerow((r[0], r[22]))
@@ -30,7 +34,7 @@ def format_scan():
 
 def main():
     export_agents_to_csv()
-    format_scan()
+    format_csv()
 
 
 if __name__ == '__main__':
