2ba2de5a252e7cdaebba8030ccdca7ffa797d362
Andres Martinez <amartinez@andymartinez.tech>
2021-04-13 11:45:01 -0400

    Removed reference to CMS site id

--- initiate_scans.py
+++ initiate_scans.py
@@ -2,13 +2,20 @@ import requests
 from os import getenv
 
 api_token = "ApiToken " + getenv('SENTINEL_ONE_API')
-CMS_SITE_ID = getenv('CMS_SITE_ID')
 headers = {'Authorization': api_token}
 
 
-def initiate_scans():
+def get_sites():
+    url = 'https://usea1-007.sentinelone.net/web/api/v2.1/sites'
+    res = requests.get(url, headers=headers, params={'limit': 1000})
+    json_res = res.json()
+    sites_ids = [site['id'] for site in json_res['data']['sites']]
+    return sites_ids
+
+
+def initiate_scans(site_ids):
     url = 'https://usea1-007.sentinelone.net/web/api/v2.1/agents/actions/initiate-scan'
-    json_data = {'filter': {'siteIds': [CMS_SITE_ID]}}
+    json_data = {'filter': {'siteIds': site_ids}}
     try:
         res = requests.post(url, headers=headers, json=json_data)
         affected = res.json()['data']['affected']
@@ -18,7 +25,8 @@ def initiate_scans():
 
 
 def main():
-    initiate_scans()
+    sites = get_sites()
+    initiate_scans(sites)
 
 
 if __name__ == '__main__':
