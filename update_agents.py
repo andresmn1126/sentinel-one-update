d5f44b410c5b9161a0be1e7c29b08c73f73200f1
Andres Martinez <amartinez@andymartinez.tech>
2021-04-16 12:26:38 -0400

    Added counter to trigger update function, renamed function to better reflect action being taken as API call is simply triggering an update

--- update_agents.py
+++ update_agents.py
@@ -19,7 +19,7 @@ def get_outdated_agents(os_type, ver):
     s = requests.session()
     url = 'https://usea1-007.sentinelone.net/web/api/v2.1/agents'
     res = s.get(url, headers=headers, params={'limit': 1000}).json()
-    agents = [a for a in res['data'] if version.parse(a['agentVersion']) < version.parse(ver)]
+    agents = [a for a in res['data'] if a['osType'] == os_type and version.parse(a['agentVersion']) < version.parse(ver)]
     while res['pagination']['nextCursor']:
         next_cursor = res['pagination']['nextCursor']
         res = s.get(url, headers=headers, params={'cursor': next_cursor, 'limit': 1000}).json()
@@ -28,24 +28,32 @@ def get_outdated_agents(os_type, ver):
     return agents
 
 
-def update_agents(agents, ver_id):
+def trigger_update(agents, ver_id):
     url = 'https://usea1-007.sentinelone.net/web/api/v2.1/agents/actions/update-software'
+    counter = 0
     for a in agents:
         try:
             json_data = {'data': {'packageId': ver_id, 'isScheduled': True}, 'filter': {'uuid': a['uuid']}}
             update = requests.post(url, headers=headers, json=json_data)
             update.raise_for_status()
             print(f"Update on {a['computerName']} under {a['siteName']} successfully triggered")
+            counter += 1
         except requests.exceptions.HTTPError as e:
             print(e)
+
+    return(counter)        
 
 
 def main():
     os_types = ('windows', 'macos')
     winid, macid = map(get_os_package, os_types)
-    winagents, macagents = map(get_outdated_agents, os_types, (winid['version'], macid['version']))
-    update_agents(winagents, winid['id'])
-    update_agents(macagents, macid['id'])
+    macagents = get_outdated_agents('macos', macid.get('version'))
+    winagents = get_outdated_agents('windows', winid.get('version'))
+    winagents_triggered = trigger_update(winagents, winid.get('id'))
+    macagents_triggered = trigger_update(macagents, macid.get('id'))
+    print(f'{winagents_triggered} Windows trigerred for update')
+    print(f'{macagents_triggered} MacOS agents triggered for update')
+
 
 
 if __name__ == '__main__':
